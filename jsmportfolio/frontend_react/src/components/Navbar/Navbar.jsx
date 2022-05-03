f61758fa097b2d791b467d7ab6a05eb57eb9a01f
Samir <Saitwalsamir@gmail.com>
2022-05-03 20:44:21 +0530

    --Navbar added

--- jsmportfolio/frontend_react/src/components/Navbar/Navbar.jsx
+++ jsmportfolio/frontend_react/src/components/Navbar/Navbar.jsx
@@ -4,15 +4,14 @@ import { images } from '../../constants';
 const Navbar = () => {
     return (
         <nav className='app__navbar'>
-            <div className='app__navbar-logo'>
-                <img src={images.logo} alt="logo"></img>
+            <div className="app__navbar-logo">
+                <img src={images.logo} alt="logo" />
             </div>
-            <ul className='app__navbar-links'>
-                {['home', 'about', 'contact', 'work', 'skills'].map((item)=>(
-                    <li className='app__flex p-text' key={`link-${item}`}>
-                        <div>
-                            <a href={`#${item}`}>{item}</a>
-                        </div>
+            <ul className="app__navbar-links">
+                {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+                    <li className="app__flex p-text" key={`link-${item}`}>
+                        <div />
+                        <a href={`#${item}`}>{item}</a>
                     </li>
                 ))}
             </ul>
