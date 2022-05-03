c7fb156f66c9c2c5501a0a3f43581e0bd883b77e
Samir <Saitwalsamir@gmail.com>
2022-05-03 19:02:53 +0530

    --Navbar Enhancement

--- jsmportfolio/frontend_react/src/components/Navbar/Navbar.jsx
+++ jsmportfolio/frontend_react/src/components/Navbar/Navbar.jsx
@@ -3,10 +3,19 @@ import './Navbar.scss';
 import { images } from '../../constants';
 const Navbar = () => {
     return (
-        <nav>
-            <div>
+        <nav className='app__navbar'>
+            <div className='app__navbar-logo'>
                 <img src={images.logo} alt="logo"></img>
             </div>
+            <ul className='app__navbar-links'>
+                {['home', 'about', 'contact', 'work', 'skills'].map((item)=>(
+                    <li className='app__flex p-text' key={`link-${item}`}>
+                        <div>
+                            <a href={`#${item}`}>{item}</a>
+                        </div>
+                    </li>
+                ))}
+            </ul>
         </nav>
     )
 }
