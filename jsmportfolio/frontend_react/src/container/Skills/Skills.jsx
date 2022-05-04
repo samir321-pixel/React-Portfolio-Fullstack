8ca883df853302a4b130dd9937fbe438e8ee57ba
Samir <Saitwalsamir@gmail.com>
2022-05-04 22:25:22 +0530

    --Experience Design

--- jsmportfolio/frontend_react/src/container/Skills/Skills.jsx
+++ jsmportfolio/frontend_react/src/container/Skills/Skills.jsx
@@ -37,13 +37,41 @@ const Skills = () => {
                             whileInView={{ opacity: [0, 1] }}
                             transition={{ duration: 0.5 }}
                             className="app__skills-item app__flex"
-                            key={skills.name}
+                            key={skill.name}
                         >
-                            <div className='app__flex' style={{ backgroundColor: skills.bgColor }}>
+                            <div className='app__flex' style={{ backgroundColor: skill.bgColor }}>
                                 <img src={urlFor(skill.icon)} alt={skill.name} />
                             </div>
                             <p className='p-text'>{skill.name}</p>
                         </motion.div>
+                    ))}
+                </motion.div>
+
+                <motion.div
+                    className='app__skills-exp'
+                >
+                    {experience.works.map((work) => (
+                        <>
+                            <motion.div
+                                whileInView={{ opacity: [0, 1] }}
+                                transition={{ duration: 0.5 }}
+                                data-tip
+                                data-for={work.name}
+                                className="app__skills-exp-work"
+                                key={work.name}
+                            >
+                                <h4 className='bold-text'>{work.name}</h4>
+                                <p className='p-text'>{work.company}</p>
+                            </motion.div>
+                            <ReactTooltip
+                                id={work.name}
+                                effect="solid"
+                                arrowColor='#fff'
+                                className='skills-tooltipl'
+                            >
+                                {work.desc}
+                            </ReactTooltip>
+                        </>
                     ))}
                 </motion.div>
             </div>
