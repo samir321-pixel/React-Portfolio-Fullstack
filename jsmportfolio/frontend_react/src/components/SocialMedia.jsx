4c93773f54d210e6a51d5ae48375affd44db44c3
Samir <Saitwalsamir@gmail.com>
2022-05-08 12:53:21 +0530

    --SocialLink Added

--- jsmportfolio/frontend_react/src/components/SocialMedia.jsx
+++ jsmportfolio/frontend_react/src/components/SocialMedia.jsx
@@ -1,18 +1,29 @@
 import React from 'react'
-import { BsTwitter, BsInstagram } from 'react-icons/bs';
+import { BsInstagram, BsGithub, BsLinkedin } from 'react-icons/bs';
 import { FaFacebookF } from 'react-icons/fa'
 
 const SocialMedia = () => {
     return (
         <div className='app__social'>
             <div>
-                <BsTwitter />
+                <a href='https://github.com/samir321-pixel' rel='noreferrer' target='_blank'>
+                    <BsGithub />
+                </a>
             </div>
             <div>
-                <BsInstagram />
+                <a href='https://www.linkedin.com/in/samir-saitwal-b517a0148/' rel='noreferrer' target='_blank'>
+                    <BsLinkedin />
+                </a>
             </div>
             <div>
-                <FaFacebookF />
+                <a href='https://www.instagram.com/chococookey/' rel='noreferrer' target='_blank'>
+                    <BsInstagram />
+                </a>
+            </div>
+            <div>
+                <a href='https://www.facebook.com/sameer.saitwal' rel='noreferrer' target='_blank'>
+                    <FaFacebookF />
+                </a>
             </div>
         </div>
     )
