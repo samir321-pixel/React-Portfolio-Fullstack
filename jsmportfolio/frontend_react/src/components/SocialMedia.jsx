44b438559bb4a0130733c1d243aa99b2004c4c47
Samir <Saitwalsamir@gmail.com>
2022-05-08 14:26:49 +0530

    --package update

--- jsmportfolio/frontend_react/src/components/SocialMedia.jsx
+++ jsmportfolio/frontend_react/src/components/SocialMedia.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BsInstagram, BsGithub, BsLinkedin } from 'react-icons/bs';
 import { FaFacebookF } from 'react-icons/fa'
-
+import './socialmedia.scss';
 const SocialMedia = () => {
     return (
         <div className='app__social'>
@@ -29,4 +29,4 @@ const SocialMedia = () => {
     )
 }
 
-export default SocialMedia
+export default SocialMedia;
